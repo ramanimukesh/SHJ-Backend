b939b5758b0817d2cbac9349d820ffc877fe53a7
Mukesh Ramani <ramani.mukesh@gmail.com>
2025-03-16 22:28:03 -0400

    MongoDB Atlas related changes

--- src/database/connect.js
+++ src/database/connect.js
@@ -1,9 +1,7 @@
 const mongoose = require("mongoose");
-mongoose.connect("mongodb://127.0.0.1:27017/Swaminarayan-Const", {
-    useNewUrlParser: true,
-    useUnifiedTopology: true,
-}).then(() => {
-    console.log("mongodb is connected");
-}).catch(() => {
-    console.log("mongodb is not connected");
-});
+mongoose.connect(process.env.MONGO_URI, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true
+})
+.then(() => console.log("Connected to MongoDB Atlas"))
+.catch(err => console.error("MongoDB connection failed:", err));
