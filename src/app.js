e2228fa5925f0b3fd76e56fa66d35496dbbbb9b2
ramanimukesh <ramani.mukesh@gmail.com>
2025-04-29 09:40:36 -0400

    add changes in a app file

--- src/app.js
+++ src/app.js
@@ -2,11 +2,12 @@ const express =  require("express");
 require("./database/connect");
 const port = process.env.PORT || 5000
 const app = express();
+const path = require("path");
 const cors = require('cors');
 
 const corsOptions = {
-    origin: ['https://shj-frontend.onrender.com', 'http://localhost:3000'],
-    credentials: true,  // Allows cookies and credentials
+    origin: ['https://shj-frontend.onrender.com', 'http://localhost:3000', 'https://sahajanandtechnologies.com'],
+    credentials: true,  
     optionsSuccessStatus: 200
 };
 
@@ -18,6 +19,11 @@ const contactRouter = require("./router/contactRouter");
 app.use(express.json());
 app.use(contactRouter);
 
+app.use(express.static(path.join(__dirname, "build")));
+
+app.get("*", (req, res) => {
+    res.sendFile(path.join(__dirname, "build", "index.html"));
+});
 
 app.listen(port,() => {
     console.log("This site port number "+port);
